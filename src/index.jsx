6d59e6da56c3bb1d921f506cb71e0c4fc3f7012d
Nohavye <noha.poncelet@gmail.com>
2023-09-20 16:13:10 +0200

    Project initialization.

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+// React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Globals Styles
+import './index.css'
+
+// Render
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <p style={{ fontFamily: 'Avenir', fontSize: '28px' }}>Argent Bank</p>
+    </React.StrictMode>
+)
